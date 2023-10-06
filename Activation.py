424c50a5acdf3b137b8cdd20f2cc60c74b5ed5bc
Gaurang Chitnis <gcc2000@hw.ac.uk>
2023-10-06 14:50:10 +0400

    fixed ReLU

--- Activation.py
+++ Activation.py
@@ -13,13 +13,15 @@ class Sigmoid(Activation):
     def evaluation(weighted_sum):
         return 1/(1+np.exp(-weighted_sum))
     def derivative(self, weighted_sum):
-       return self.evaluation(self,weighted_sum) * (1 - self.evaluation(self,weighted_sum))
+       return self.evaluation(weighted_sum) * (1 - self.evaluation(weighted_sum))
         
 class ReLU(Activation):
     def evaluation(weighted_sum):
-        return np.maximum(0,weighted_sum)
+        return weighted_sum * (weighted_sum > 0)
     def derivative(self, x):
-        return np.where(x>0,1,0)
+        x[x<=0] = 0
+        x[x>0] = 1
+        return x
     
 class TanH(Activation):
     def evaluation(weighted_sum):
@@ -30,8 +32,7 @@ class TanH(Activation):
 print("1." ,Sigmoid.derivative(Sigmoid,np.array([5,7])))
 print("2. " ,ReLU.derivative(ReLU,np.array([5,7])))
 print("3. " ,TanH.derivative(TanH,np.array([5,7])))
-
-
+print(ReLU.derivative(ReLU,np.array([1,-2,3,4])))
 '''
 References
 1. https://dustinstansbury.github.io/theclevermachine/derivation-common-neural-network-activation-functions
