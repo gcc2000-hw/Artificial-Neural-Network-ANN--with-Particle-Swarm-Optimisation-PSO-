2862c15b9c2cb5ffbd8ec3bc00d867aa11bdaf40
nrt2000 <nrt2000@hw.ac.uk>
2023-10-06 15:44:59 +0400

    implemented ANNBuilder

---
+++ NetworkBuilder.py
@@ -0,0 +1,9 @@
+from Network import Network
+from Layer import Layer
+
+class ANNBuilder:
+    def build(nb_layers, list_nodes, list_functions):
+        ann = Network()
+        for i in range(nb_layers):
+            layer = Layer(list_nodes[i], list_functions[i])
+            ann.append(layer)
