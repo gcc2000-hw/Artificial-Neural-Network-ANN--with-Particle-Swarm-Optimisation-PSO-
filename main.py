9104a03ce3eb31acc15ade033b9c25910955645f
nrt2000 <nrt2000@hw.ac.uk>
2023-10-12 18:35:24 +0400

    dataset

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import numpy as np
 from Activation import *
 from Loss import *
+import pandas as pd;
+
+UCI_auth_data = pd.read_csv()
 
 
-
-
-'''
-References
-1. https://dustinstansbury.github.io/theclevermachine/derivation-common-neural-network-activation-functions
-
-'''
