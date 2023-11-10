4f81edda054149c9e87ed0b69df00f1a4940b136
nrt2000 <nrt2000@hw.ac.uk>
2023-11-10 16:48:51 +0400

    structure for PSO

---
+++ Particle.py
@@ -0,0 +1,21 @@
+class Particle:
+    def __init__(self):
+        self.p_best= 0
+        self.l_best=0
+        self.g_best=0
+        self.position = 0
+        self.velocity = 0
+        self.neighbours = []
+
+    def get_neighbours():
+        pass
+
+    def update_velocity():
+        pass
+
+    def update_position():
+        pass
+
+    # calculate loss and update l_best, p_best and g_best
+    def get_loss():
+        pass
---
+++ Population.py
@@ -0,0 +1,8 @@
+class Population:
+    def __init__(self):
+        self.particles =[]
+
+    # Add a new particle to the population
+    def add_particle(self, particle):
+        pass
+
